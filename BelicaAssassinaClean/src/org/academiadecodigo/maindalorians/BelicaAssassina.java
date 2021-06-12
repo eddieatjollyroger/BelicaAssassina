8bcd153a54e15cb523dfd5ee68de039f514df649
eddieatjollyroger <38026492+eddieatjollyroger@users.noreply.github.com>
2021-06-12 22:46:49 +0000

    Belica Assassina Cleaner

---
+++ BelicaAssassinaClean/src/org/academiadecodigo/maindalorians/BelicaAssassina.java
@@ -0,0 +1,16 @@
+package org.academiadecodigo.maindalorians;
+
+
+public class BelicaAssassina {
+
+
+    public static void main(String[] args) {
+
+        Game game = new Game();
+        game.startScreen();
+        game.init();
+    }
+}
+
+
+
